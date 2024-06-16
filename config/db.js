115c21aca1f388945e2661d62e1faa0d0e2e4004
SteveDev1 <mobemobesh28@gmail.com>
2024-06-16 13:15:06 +0300

    debug

--- config/db.js
+++ config/db.js
@@ -7,12 +7,4 @@ const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${p
 // Parse the URL to extract connection parameters
 const connection = mysql.createConnection(urlDB);
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
-    }
-    console.log('Connected to the database as id', connection.threadId);
-});
-
 module.exports = connection;
