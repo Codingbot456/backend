d53e23ee3e739db8f58a7e2ceb8eef50b55020ce
SteveDev1 <mobemobesh28@gmail.com>
2024-06-16 16:36:33 +0300

    added ci

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const urlDb = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+
+// Create a connection using the connection URL
+const connection = mysql.createConnection(urlDb);
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database as id', connection.threadId);
+});
+
+module.exports = connection;
