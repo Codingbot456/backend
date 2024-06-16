d7b4aca4aa64f8ac7a41070b02c5bf50acc8cd71
SteveDev1 <mobemobesh28@gmail.com>
2024-06-16 11:07:35 +0300

    updated the connection

--- config/db.js
+++ config/db.js
@@ -4,15 +4,26 @@ require('dotenv').config();
 // Create the connection URL using string interpolation
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT || 3306}/${process.env.MYSQLDATABASE}`;
 
-// Parse the URL to extract connection parameters
-const connection = mysql.createConnection(urlDB);
+// Parse the URL to extract connection parameters using URL module
+const { hostname: host, port, username: user, password, pathname } = new URL(urlDB);
+const database = pathname.split('/')[1];
 
-connection.connect((err) => {
+const pool = mysql.createPool({
+    host,
+    user,
+    password,
+    database,
+    port: port || 3306, // Default MySQL port is 3306
+    connectionLimit: 10 // Adjust based on your needs
+});
+
+pool.getConnection((err, connection) => {
     if (err) {
         console.error('Error connecting to the database:', err.stack);
         return;
     }
-    console.log('Connected to the database as id', connection.threadId);
+    console.log('Connected to the database');
+    connection.release();
 });
 
-module.exports = connection;
+module.exports = pool;
