edf3b1e17d10ea698da719f400bcb57d7b7e51c9
SteveDev1 <mobemobesh28@gmail.com>
2024-06-16 15:11:55 +0300

    solved crashing

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -6,14 +7,14 @@ require('dotenv').config();
 const app = express();
 const PORT = 7000;
 
-// Require MySQL connection
-const db = require('./config/db'); // Adjust the path as needed
-
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const apiRouter = require('./routes/api');
 const orderRoutes = require('./routes/orderRoutes');
 const searchRoutes = require('./routes/searchRoutes');
+
+
+  
 
 // Middleware
 app.use(bodyParser.json());
@@ -21,9 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
-
-// Attach MySQL connection to app.locals for access in routes
-app.locals.db = db;
 
 // Routes
 app.use('/api', productRoutes);
