e2c8273a28a8df05cb3dd747763e0896dec3506b
SteveDev1 <mobemobesh28@gmail.com>
2024-06-16 13:29:22 +0300

    new

--- config/db.js
+++ config/db.js
@@ -4,15 +4,18 @@ require('dotenv').config();
 // Create the connection URL using string interpolation
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT || 3306}/${process.env.MYSQLDATABASE}`;
 
-// Parse the URL to extract connection parameters
+// Create the MySQL connection
 const connection = mysql.createConnection(urlDB);
 
+// Connect to the database
 connection.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err.stack);
         return;
     }
+    console.log('Connected to server');
     console.log('Connected to the database as id', connection.threadId);
 });
 
+// Export the connection for external usage
 module.exports = connection;
